f8d70a0eba9111638e1ed0570d02e408490bf384
ChrisScheffler <>
2018-04-03 02:09:58 +0200

    - added queryAll method - added debug information on discovery start/stop - adjusted sample

--- sample.js
+++ sample.js
@@ -1,12 +1,20 @@
 const miflora = require('./lib/miflora.js');
 
-miflora.discover(5000).then(devices => {
+miflora.discover(10000).then(devices => {
 	console.log('done', devices);
+	// query all
+	miflora.queryAll().then(data => {
+		console.log(data);
+	})
+
+	// query first
+/*	
 	miflora.queryDevice(devices[0]).then(data => {
 		console.dir(data);
 	}).catch(e => {
 		console.log('query error', e);
 	});
+*/
 }).catch(e => {
 	console.log('error:', e);
 });
