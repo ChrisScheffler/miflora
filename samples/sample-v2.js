1ba4ec4fd4b5382887f6464c7c225b11b3dacb06
Christian Scheffler <1375050+ChrisScheffler@users.noreply.github.com>
2018-04-09 03:21:27 +0200

    refactor

--- samples/sample-v2.js
+++ samples/sample-v2.js
@@ -1,21 +1,9 @@
 'use strict';
 
-const MiFlora = require('../lib/miflora2.js');
+const miflora = require('../lib/miflora.js');
 
-const miflora = new MiFlora();
-
-miflora.discover({ timeout: 5000 }).then(devices => {
-	for (const device of devices) {
-		device.connect().then(() => {
-			device.queryFirmwareInfo().then(firmwareData => {
-				console.log(firmwareData);
-				device.querySensorInfo().then(sensorData => {
-					console.log(sensorData);
-				});
-			});
-		});
-	}
-	console.log('done');
+miflora.discover({ duration: 5000 }).then(devices => {
+	console.dir(devices);
 }).catch(err => {
 	console.error('error while discovering', err);
 });
