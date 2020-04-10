c3186040870323dd38f8446fcb7c6c43a571d2cd
Chris <>
2020-04-10 17:29:10 +0200

    - added blink() and reset() methods to device
    - added fix for empty addresses on discovery
    
    - changed noble links to abandonware

--- samples/sample-query.js
+++ samples/sample-query.js
@@ -1,19 +1,19 @@
 'use strict';
 
-const miflora = require('../lib/miflora.js');
+const miflora = require('../lib/miflora');
 
 const discoverOptions = {
-	addresses: ['c4:7c:8d:65:d5:26'],
+	addresses: ['c4:7c:8d:66:c6:41'],
 	ignoreUnknown: true,
-	duration: 30000
+	duration: 10000
 };
 
 (async function () {
 	console.log('> scanning for a max of %s seconds', discoverOptions.duration / 1000);
 	const devices = await miflora.discover(discoverOptions);
-	const device = devices.find(entry => entry.address === 'c4:7c:8d:65:d5:26');
+	const device = devices.find(entry => entry.address === 'c4:7c:8d:66:c6:41');
 	if (device) {
-		await device.query();
+		console.log(await device.query());
 	} else {
 		console.log('not found');
 	}
